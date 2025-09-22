ccac51f221993aff9e6e8c572029eed63f51cec0
Augustus Dagbay <augustusdagbay@gmail.com>
2025-09-22 20:08:11 +0800

    Implement ApplicationDbContext Add Table Configuration Add Initital Migration

---
+++ Services/Learning.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Learning.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learning.Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Property> Properties { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Services/Learning.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Learning.Application.Data;
+using Learning.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Learning.Infrastructure
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options) { }
+        public DbSet<Property> Properties => Set<Property>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}

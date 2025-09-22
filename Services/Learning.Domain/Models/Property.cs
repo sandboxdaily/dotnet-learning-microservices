495c2fd674e1b6fc1604d10dba5508e732058e7d
Augustus Dagbay <augustusdagbay@gmail.com>
2025-09-22 19:23:23 +0800

    Implementation of Clean Architecture and CQRS using mediatr

---
+++ Services/Learning.Domain/Models/Property.cs
@@ -0,0 +1,23 @@
+﻿using Learning.Domain.Abstractions;
+
+namespace Learning.Domain.Models
+{
+    public class Property : Entity<Guid>
+    {
+        public string Name { get; private set; } = default!;
+
+        public static Property Create(Guid id, string name)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+            var property = new Property
+            {
+                Id = id,
+                Name = name
+            };
+
+            return property;
+        }        
+    }
+
+}

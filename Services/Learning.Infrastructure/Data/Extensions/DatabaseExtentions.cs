5ed073c01b6be7f528b80206812877e0c77d86ab
Augustus Dagbay <augustusdagbay@gmail.com>
2025-09-26 21:08:31 +0800

    Database migration ..creation of database

---
+++ Services/Learning.Infrastructure/Data/Extensions/DatabaseExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Learning.Infrastructure.Data.Extensions
+{
+    public static class DatabaseExtentions
+    {
+        public static async Task InitialiseDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            //await SeedAsync(context);
+        }
+    }
+}
